157d062f9dd19d11f72c5539b95ed8207a99f6e8
MdAbdulKayumKhan <kayum261@gmail.com>
2021-09-21 12:23:44 +0600

    folder structure using and added react extention pack

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
+import Header from './components/Header/Header';
+import Person from './components/Person/Person';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Countries></Countries>
+      <Person></Person>
     </div>
   );
 }
 
-function Countries(){
-  const [countries, setCountries] = useState([]);
-  useEffect(()=>{
-    fetch('https://restcountries.eu/rest/v2/all')
-    .then(res => res.json())
-    .then(data => setCountries(data))
-  }, [])
-  return (
-    <dir>
-      <h2>Traveling around the world!!</h2>
-      <h4>Countries Available: {countries.length} </h4>
-    </dir>
-  )
-}
 
 export default App;
